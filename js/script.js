e4e6e2b3ee6048b7f36ec20082f6dffcb18f029c
Lukasz Smolnicki <lukasz.smolnicki@gmail.com>
2020-09-03 17:01:27 +0200

    Section about and future added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+$(document).ready(function () {
+    $('a[href^="#"]').click(function () {
+        let hash = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 1000);
+        return false;
+    });
+});
 $('#myTab a').on('click', function (e) {
     e.preventDefault()
     $(this).tab('show')
